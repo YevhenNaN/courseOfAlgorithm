7ff899706da77e2326bf3f60570305929b56c4e1
YevhenZzz <44346100+YevhenZzz@users.noreply.github.com>
2019-12-02 01:46:03 +0200

    Update poker.js

--- lab2_algotiyhms/poker.js
+++ lab2_algotiyhms/poker.js
@@ -1,24 +1,67 @@
-function merge_sort(left_part,right_part)
-{
-    var i = 0;
-    var j = 0;
-    var results = [];
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
-    while (i < left_part.length || j < right_part.length) {
-        if (i === left_part.length) {
-            // j is the only index left_part
-            results.push(right_part[j]);
-            j++;
+class Main {
+    public static void main(String args[]) throws FileNotFoundException {
+        File file = new File("//home/lenovo/Завантаження/idea-IU-192.7142.36//tall.txt");
+        Scanner scanner = new Scanner(file);
+
+
+        int lengthOfNumbers = 0;
+        while (scanner.hasNextInt()) {
+            scanner.nextInt();
+            ++lengthOfNumbers;
         }
-        else if (j === right_part.length || left_part[i] <= right_part[j]) {
-            results.push(left_part[i]);
-            i++;
-        } else {
-            results.push(right_part[j]);
-            j++;
+        scanner.close();
+
+        Scanner c = new Scanner(file);
+
+        int[] cardArray = new int[lengthOfNumbers];
+
+        int indexOfNumberInFile = 0;
+        while (indexOfNumberInFile < lengthOfNumbers) {
+            cardArray[indexOfNumberInFile] = c.nextInt();
+            indexOfNumberInFile++;
         }
+
+        for (int runByIteratorOfArray : cardArray) {
+
+            System.out.println(runByIteratorOfArray);
+        }
+
+        int amountOfZeros = 0;
+        for (int runByIteratorOfArray : cardArray) {
+            if (runByIteratorOfArray == 0) ++amountOfZeros;
+        }
+        System.out.println("Jokers: " + amountOfZeros);
+
+        int longestCombination = 0;
+
+        for (int startNumber = 0; startNumber < lengthOfNumbers; ++startNumber) {
+            int firstNumber = cardArray[startNumber];
+            int lengthOfCombination = 0;
+            int localAmountOfJokers = amountOfZeros;
+            for (int nextNumberForCombiination = 1; nextNumberForCombiination < lengthOfNumbers; ++nextNumberForCombiination) {
+                int lookingNumber = cardArray[startNumber] + nextNumberForCombiination;
+                boolean found = false;
+                for (int runByIteratorOfArray : cardArray) {
+                    if (runByIteratorOfArray == lookingNumber) found = true;
+                }
+                if (found) {
+                    lengthOfCombination++;
+                } else {
+                    if (localAmountOfJokers == 0) break;
+                    localAmountOfJokers--;
+                    lengthOfCombination++;
+                }
+            }
+
+            if (longestCombination < lengthOfCombination) longestCombination = lengthOfCombination;
+
+        }
+
+        System.out.println("Longest combination:" + (longestCombination));
+
     }
-    return results;
 }
-
-console.log(merge_sort([1,3,4], [3,7,9]));

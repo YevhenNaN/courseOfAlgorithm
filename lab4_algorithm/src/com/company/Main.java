339ed657bb230f434c41ce5adf09ec9e2f7f2afb
YevhenZzz <44346100+YevhenZzz@users.noreply.github.com>
2019-12-03 16:52:16 +0200

    Update Main.java

--- lab4_algorithm/src/com/company/Main.java
+++ lab4_algorithm/src/com/company/Main.java
@@ -8,73 +8,73 @@ import java.util.List;
 import java.util.Scanner;
 
 
-class Connection{
+class Connection {
     public int Node1;
     public int Node2;
     public int ConnectionTime;
 
-    public Connection(int a, int b, int c){
-        Node1=a;
-        Node2=b;
-        ConnectionTime=c;
+    public Connection(int a, int b, int c) {
+        Node1 = a;
+        Node2 = b;
+        ConnectionTime = c;
     }
 }
-
 
 
 public class Main {
 
     public static int[] getData(File file) throws FileNotFoundException {
-        Scanner scan = new Scanner(file);
+        Scanner scanner = new Scanner(file);
         int[] dataArray = new int[150];
         int i = 0;
-        while(scan.hasNextInt()) {
-            dataArray[i] = scan.nextInt();
+        while (scanner.hasNextInt()) {
+            dataArray[i] = scanner.nextInt();
             i++;
         }
         return dataArray;
     }
-    public static int sum=0;
+
+    public static int sumOfMaxPing = 0;
+
     //*************************************************************************************
-    public static int FindConnectionSum(int ClientNode, ArrayList<Connection> Connections, int AmountOfNodes, int AmountOfConnections, int serverplace) {
-        if (ClientNode != serverplace) {
+    public static int FindConnectionSum(int clientNode, ArrayList<Connection> connections, int amountOfNodes, int amountOfconnections, int serverPlace) {
+        if (clientNode != serverPlace) {
             boolean isFirstToServer = false;
 
             int nextnode = 0;
             int minimaltime = 100000;
 
-            for (int i = 0; i < AmountOfConnections; ++i) {
-                if (Connections.get(i).Node1 == ClientNode || Connections.get(i).Node2 == ClientNode) {
-                    if (Connections.get(i).Node1 == serverplace || Connections.get(i).Node2 == serverplace) {
+            for (int i = 0; i < amountOfconnections; ++i) {
+                if (connections.get(i).Node1 == clientNode || connections.get(i).Node2 == clientNode) {
+                    if (connections.get(i).Node1 == serverPlace || connections.get(i).Node2 == serverPlace) {
                         isFirstToServer = true;
 
-                        sum =sum+ Connections.get(i).ConnectionTime;
-                        return sum;
+                        sumOfMaxPing = sumOfMaxPing + connections.get(i).ConnectionTime;
+                        return sumOfMaxPing;
 
                     } else {
 
                         boolean help = false;
-                        if (Connections.get(i).Node1 == ClientNode) {
+                        if (connections.get(i).Node1 == clientNode) {
 
-                            for (int k = 0; k < AmountOfConnections; ++k) {
+                            for (int k = 0; k < amountOfconnections; ++k) {
 
                                 if (k != i) {
-                                    if (Connections.get(k).Node1 == Connections.get(i).Node2 || Connections.get(k).Node2 == Connections.get(i).Node2) {
-                                        if (minimaltime > Connections.get(k).ConnectionTime) {
+                                    if (connections.get(k).Node1 == connections.get(i).Node2 || connections.get(k).Node2 == connections.get(i).Node2) {
+                                        if (minimaltime > connections.get(k).ConnectionTime) {
                                             help = true;
-                                            minimaltime = Connections.get(k).ConnectionTime;
+                                            minimaltime = connections.get(k).ConnectionTime;
                                         }
                                     }
                                 }
                             }
-                            if (help = true)
-                                if (Connections.get(i).Node1 == ClientNode)
-                                    nextnode = Connections.get(i).Node2;
+                            if (connections.get(i).Node1 == clientNode)
+                                nextnode = connections.get(i).Node2;
 
                         } else {
                             if (help = true)
-                                if (Connections.get(i).Node2 == ClientNode)
-                                    nextnode = Connections.get(i).Node1;
+                                if (connections.get(i).Node2 == clientNode)
+                                    nextnode = connections.get(i).Node1;
 
 
                         }
@@ -84,81 +84,84 @@ public class Main {
 
                 }
             }
-            for (int i = 0; i < AmountOfConnections; ++i) {
-                if (Connections.get(i).Node1 == nextnode && Connections.get(i).Node2 == ClientNode) {
-                    sum += Connections.get(i).ConnectionTime;}
-                else if(Connections.get(i).Node2 == nextnode && Connections.get(i).Node1 == ClientNode){
-                    sum += Connections.get(i).ConnectionTime;}
+            for (int i = 0; i < amountOfconnections; ++i) {
+                if (connections.get(i).Node1 == nextnode && connections.get(i).Node2 == clientNode) {
+                    sumOfMaxPing += connections.get(i).ConnectionTime;
+                } else if (connections.get(i).Node2 == nextnode && connections.get(i).Node1 == clientNode) {
+                    sumOfMaxPing += connections.get(i).ConnectionTime;
+                }
             }
 
-            if (nextnode != serverplace)
-                FindConnectionSum(nextnode, Connections, AmountOfNodes, AmountOfConnections, serverplace);
+            if (nextnode != serverPlace)
+                FindConnectionSum(nextnode, connections, amountOfNodes, amountOfconnections, serverPlace);
         }
 
-        return sum;
+        return sumOfMaxPing;
     }
 
 
     public static void main(String[] args) throws FileNotFoundException {
-        int AmountOfNodes;
-        int AmountOfConnections;
+        int amountOfNodes;
+        int amountOfconnections;
 
         File file = new File("/home/lenovo/Завантаження/gamsrv.txt");
         int[] Array = getData(file);
 
-        int AmountOfInts=0;
+        int AmountOfInts = 0;
 
-        for(int i=0; i<Array.length;++i){
-            if(Array[i]==0){
-                AmountOfInts=i;
+        for (int i = 0; i < Array.length; ++i) {
+            if (Array[i] == 0) {
+                AmountOfInts = i;
                 break;
             }
             //System.out.println(" "+ Array[i]);
         }
-        AmountOfNodes=Array[0];
-        AmountOfConnections=Array[1];
-        System.out.println("Amount of nodes:"+AmountOfNodes);
-        System.out.println("Amount of Connections:"+AmountOfConnections);
+        amountOfNodes = Array[0];
+        amountOfconnections = Array[1];
+        System.out.println("Amount of nodes:" + amountOfNodes);
+        System.out.println("Amount of Connections:" + amountOfconnections);
 
-        int[] ClientNodes= new int[3];
-        ClientNodes[0]=Array[2];
-        ClientNodes[1]=Array[3];
-        ClientNodes[2]=Array[4];
+        int[] clientNodes = new int[3];
+        clientNodes[0] = Array[2];
+        clientNodes[1] = Array[3];
+        clientNodes[2] = Array[4];
 
 
         ArrayList<Connection> Connections = new ArrayList<Connection>();
 
-        for(int i=5;i<AmountOfInts;i+=3){
-            Connection a=new Connection(Array[i],Array[i+1],Array[i+2]);
+        for (int i = 5; i < AmountOfInts; i += 3) {
+            Connection a = new Connection(Array[i], Array[i + 1], Array[i + 2]);
             Connections.add(a);
         }
 
-        for(int i=0; i<3;++i) {
-            System.out.println("ClientNode:"+ClientNodes[i]);
+        for (int i = 0; i < 3; ++i) {
+            System.out.println("clientNode:" + clientNodes[i]);
         }
-        for(int i=0; i<Connections.size();++i) {
+        for (int i = 0; i < Connections.size(); ++i) {
             System.out.println(Connections.get(i).Node1 + " " + Connections.get(i).Node2 + " " + Connections.get(i).ConnectionTime);
         }
 
 
+        int usermax = 0;
+        int max = 0;
 
-        int usermax=0; int max=0;
+        for (int j = 1; j <= amountOfNodes; ++j) {
+            for (int i = 0; i < 3; ++i) {
+                if (j != clientNodes[i]) {
+                    FindConnectionSum(clientNodes[i], Connections, amountOfNodes, amountOfconnections, j);
 
-        for (int j = 1; j <= AmountOfNodes; ++j) {
-            for(int i=0; i<3;++i) {
-                if(j!=ClientNodes[i]) {
-                    FindConnectionSum(ClientNodes[i], Connections, AmountOfNodes, AmountOfConnections, j);
+                    if (sumOfMaxPing > usermax) usermax = sumOfMaxPing;
 
-                    if(sum>usermax)usermax=sum;
-
-                    sum = 0;
+                    sumOfMaxPing = 0;
                 }
 
             }
-            if(max<usermax){max=usermax;}
+            if (max < usermax) {
+                max = usermax;
+            }
         }
 
-        System.out.println("max:"+max);
+        System.out.println("max:" + max);
 
     }
 
